3eb9c37b8c4ca45a69c656861b693fd7df522413
arisboo <arisboo@ncsu.edu>
2019-04-23 16:21:51 +0000

    adding pm2 and dockerfile

--- index.js
+++ index.js
@@ -4,24 +4,28 @@ const path = '/etc/kubernetes/admin.conf'
 const config = require('kubernetes-client').config
 var app=express();
 
-const deploymentManifest = require('./bluegreen.json')
+const blue = require('./blue.json')
+const green = require('./green.json')
 
 async function applyDeploy () {
-  const client = new Client({ config: config.fromKubeconfig(), version: '1.9' })
+  const client = new Client({ config: config.fromKubeconfig(path), version: '1.9' })
 
   try {
-    const create = await client.apis.apps.v1.namespaces('default').deployments.post({ body: deploymentManifest })
-    console.log('Create:', create)
+    const create_blue = await client.apis.apps.v1.namespaces('default').deployments.post({ body: blue })
+    const create_green = await client.apis.apps.v1.namespace('default').deployments.post({body: green})
+    console.log('Create:', create_blue, create_green)
   } catch (err) {
     if (err.code !== 409) throw err
-    await client.apis.apps.v1.namespaces('default').deployments(deploymentManifest.metadata.name).delete()
-    const replace = await client.apis.apps.v1.namespaces('default').deployments.post({ body: deploymentManifest }) 
-    console.log('Replace:', replace)
+    await client.apis.apps.v1.namespaces('default').deployments(blue.metadata.name).delete()
+    await client.apis.apps.v1.namespaces('default').deployments(green.metadata.name).delete()
+    const replace_blue = await client.apis.apps.v1.namespaces('default').deployments.post({ body: blue })
+    const replace_green = await client.apis.apps.v1.namespace('default').deployments.post({body: green}) 
+    console.log('Replace:', replace_blue, replace_green)
   }
 }
 
 app.post("/hook", (req, res, next) => {
-    const webhookUrl = req.params.url;
+    const webhookUrl = req.params;
     console.log("hit")
     applyDeploy() 
     res.status(200).send('OK')
